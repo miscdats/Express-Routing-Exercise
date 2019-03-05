673f397b32abc567e7e5ec1d2ee1c5b6c08ffb45
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-04 23:59:52 -0800

    2

--- routes/route2.js
+++ routes/route2.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var colors = require('../colors.json');
 
+router.get('/', function(req, res, next) {
+  res.render('route2', { title: 'AD320: IA7-Taupier Route 2' });
+});
 
-router.get('/:color-name', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/all', function(req, res, next) {
+  res.json(colors);
+});
+
+router.get('/:aColor', function(req, res, next) {
+  for (key in colors[0]) {
+    if (colors[0].color == ('aColor')) {
+      res.send(req.params.aColor);
+    }
+  }
 });
 
 module.exports = router;
