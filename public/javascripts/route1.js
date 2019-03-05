d59408cdf9784bb42f2a7a228947b0e796be19dc
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-04 21:11:58 -0800

    script adjust

--- public/javascripts/route1.js
+++ public/javascripts/route1.js
@@ -1,54 +1,17 @@
 $(function() {
 
-  $("redplace").on("click", function() {
-    if($(this).css('background-color')=='red') {
-      $(this).css({
-        backgroundColor: 'blue',
-        border: '10px dashed red',
-        color: 'red'
-      });
-      $(this).html('BLUE');
-    } else if ($(this).css('background-color')=='blue') {
-      $(this).css({
-        backgroundColor: 'red',
-        border: '10px dashed blue',
-        color: 'blue'
-      });
+  $(".redplace").toggle( function() {
+      $(this).css('background', 'red');
+      $(this).css('border', '10px dashed blue');
+      $(this).css('color', 'blue');
       $(this).html('RED');
-    }    
-  });
-
-  function colorChange() {
-    var bgColor = document.getElementById("redplace").style.backgroundColor;
-    var h3Color = document.getElementById("redplace").style.color;
-    var borderColor = document.getElementById("redplace").style.border;
-    var text = document.getElementById("redplace").innerHTML;
-
-    if (bgColor == "red") {
-      bgColor = "blue";
-      h3Color = "red";
-      borderColor = "10px dashed red";
-      text = "BLUE";
-    } else if (bgColor == "blue") {
-      bgColor = "red";
-      h3Color = "blue";
-      borderColor = "10px dashed blue";
-      text = "RED";
+    }, function() {
+      $(this).css('background', 'blue');
+      $(this).css('border', '10px dashed red');
+      $(this).css('color', 'red');
+      $(this).html('BLUE');
     }
-    document.getElementById("redplace").style.backgroundColor = bgColor;
-    document.getElementById("redplace").style.color = h3Color;
-    document.getElementById("redplace").style.border = borderColor;
-    document.getElementById("redplace").innerHTML = text;
-  }
+  );
 
-  try {
-      console.log("getting it..");
-      var redplace = document.getElementById("redplace");
-      console.log(redplace);
-      redplace.addEventListener("click", colorChange);
-      console.log("maybe!");
-  } catch(err) {
-      console.log(err);
-  }
 
 });
