4f96d2f3df708dc06f6adb9e35b87a05a081c96b
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-04 22:15:47 -0800

    fixed

--- public/javascripts/route1.js
+++ public/javascripts/route1.js
@@ -1,17 +1,12 @@
 $(function() {
 
-  $(".redplace").toggle( function() {
-      $(this).css('background', 'red');
-      $(this).css('border', '10px dashed blue');
-      $(this).css('color', 'blue');
-      $(this).html('RED');
-    }, function() {
-      $(this).css('background', 'blue');
-      $(this).css('border', '10px dashed red');
-      $(this).css('color', 'red');
-      $(this).html('BLUE');
-    }
-  );
+  console.log('before');
 
+  $('#place').click( function() {
+    $(this).toggleClass('blueplace');
+    console.log('during');
+  });
+
+  console.log('after');
 
 });
